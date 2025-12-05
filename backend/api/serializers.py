ce91ef39caca3876229f790ef78d35f27d3ee7b9
vlad <Vlad>
2025-12-05 17:40:41 +0300

    add serializers & views

---
+++ backend/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from recipes.models import Ingredient, Tag
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from recipes.models import Ingredient, Tag
+from .serializers import IngredientSerializer, TagSerializer
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    pagination_class = None  # Для тегов пагинация обычно не нужна, их мало
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    pagination_class = None  # Отключаем пагинацию для ингредиентов
+    search_fields = ('^name',)  # Поиск по началу названия
